8220f27ca45cf68b76a093083f21806f522154d8
Thomas Brown <github@honourity.com>
2017-12-03 20:12:57 +1100

    added alert colour indicator to timer dial as it runs out

--- Assets/_Scripts/Controllers/TimerController.cs
+++ Assets/_Scripts/Controllers/TimerController.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerController : MonoBehaviour
 {
@@ -7,7 +8,14 @@ public class TimerController : MonoBehaviour
    private RectTransform _hand;
    [SerializeField]
    private float _timerLength = 30f;
+   [SerializeField]
+   private float _fullRevolutionTime = 60f;
+   [SerializeField]
+   float timeLeftStartColourChange = 10f;
+   [SerializeField]
+   float timeLeftSolidColour = 1f;
 
+   private Image _faceSprite;
    private float _timerSeconds;
    private bool _runTimer;
 
@@ -25,6 +33,7 @@ public class TimerController : MonoBehaviour
    {
       _timerSeconds = 0f;
       _runTimer = true;
+      StartCoroutine(TimerColourShift());
    }
 
    private void Update()
@@ -38,8 +47,43 @@ public class TimerController : MonoBehaviour
          }
          else
          {
-            _hand.localRotation = Quaternion.Euler(_hand.localRotation.eulerAngles.x, _hand.localRotation.eulerAngles.y, -360f * (_timerSeconds / 60f));
+            _hand.localRotation = Quaternion.Euler(_hand.localRotation.eulerAngles.x, _hand.localRotation.eulerAngles.y, -360f * (_timerSeconds / _fullRevolutionTime));
          }
       }
+   }
+
+   private void Awake()
+   {
+      _faceSprite = GetComponent<Image>();
+   }
+
+   private IEnumerator TimerColourShift()
+   {
+      float colourIntensity = 1f;
+      var timeLeft = _timerLength - _timerSeconds;
+
+      _faceSprite.color = new Color(_faceSprite.color.r, colourIntensity, colourIntensity);
+
+      while (_runTimer
+         && timeLeft > timeLeftSolidColour
+         && _timerSeconds < _timerLength)
+      {
+         timeLeft = _timerLength - _timerSeconds;
+
+         if (timeLeft < timeLeftStartColourChange)
+         {
+            //move the colour back and forth based on time past and some tweaked scaling
+            var modifiedTime = (_timerSeconds * (1 / timeLeft)) + 1f;
+            colourIntensity = Mathf.PingPong(modifiedTime, 1f);
+
+            //apply colour
+            _faceSprite.color = new Color(_faceSprite.color.r, colourIntensity, colourIntensity);
+         }
+
+         yield return new WaitForEndOfFrame();
+      }
+
+      _faceSprite.color = new Color(_faceSprite.color.r, 0f, 0f);
+      yield return null;
    }
 }
